4c1aa8203d1236fd57943ddc204204f2657cc19f
Frinz1 <119020211+Frinz1@users.noreply.github.com>
2024-02-29 12:29:30 +0800

    w

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,10 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        
+        '/login',
+        '/login',
+        '/register',
+        '/logout'
         
     ];
 }
